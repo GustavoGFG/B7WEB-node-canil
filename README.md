6a7574abbf5f5538b31e5d64c228e31b953a86c0
Gustavo Fernandes <goncalves_017@hotmail.com>
2023-11-14 11:09:47 -0300

    base criada

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # B7WEB-node-canil
+
+### Pré-requisitos globais:
+
+`npm i -g nodemon`
+
+### Instalação
+
+`npm install`
+
+### Para rodar o projeto
+
+`npm run start-dev`
---
+++ src/controllers/pageController.js
@@ -0,0 +1,13 @@
+export const home = (req, res) => {
+  res.send('home no controller');
+  //res.render('pages/page');
+};
+export const dogs = (req, res) => {
+  //res.render('pages/page');
+};
+export const cats = (req, res) => {
+  //res.render('pages/page');
+};
+export const fishes = (req, res) => {
+  //res.render('pages/page');
+};
